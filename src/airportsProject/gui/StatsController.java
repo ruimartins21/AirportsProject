7976a79bce7e81969c7a57901a4120b3e0933b6c
RicardoBarbosa <ricardopbarbosaa@gmail.com>
2017-05-14 00:23:58 +0100

    to merge

--- src/airportsProject/gui/StatsController.java
+++ src/airportsProject/gui/StatsController.java
@@ -1,6 +1,9 @@
 package airportsProject.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.XYChart;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
@@ -10,6 +13,16 @@ public class StatsController {
     private HBox containCharts;
     @FXML
     private ImageView goBack;
+
+    public void initialize(){
+
+    }
+
+    private ObservableList<XYChart.Series<String, Double>> getMostrTrafficAirport(){
+        ObservableList<XYChart.Series<String, Double>> data = FXCollections.observableArrayList();
+
+        return data;
+    }
 
     @FXML
     void optionClicked(MouseEvent event){
